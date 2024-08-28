79f0b03c93ac7fd7ae7bb7ee07cbfe6502bf1802
Lucas Bruder <110999721+Bruder1z@users.noreply.github.com>
2024-08-28 11:17:52 -0300

    Add files via upload

---
+++ Agenda/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Agenda.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Agenda.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public DbSet<Contato> Contatos {get; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) 
+        => options.UseSqlite("DataSource=app.db;Cache=Shared");
+    }
+}
